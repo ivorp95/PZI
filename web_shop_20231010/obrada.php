06648958bab0b7ba62a5f6c07ef40eec984c9b04
ivorp95 <ivor.pangos@live.com>
2023-11-07 16:18:21 +0100

    modified:   obrada.php

--- web_shop_20231010/obrada.php
+++ web_shop_20231010/obrada.php
@@ -4,6 +4,6 @@
 <?php
 $email=$_GET['email'];
 $poruka=$_GET['poruka'];
-echo"<h1>Pozdrav!".$email. "<!h1>";
+echo"<h1>Pozdrav ja sam PHP server!".$email. "<!h1>";
 echo"<h1>Poruka:".$poruka. "<!h1>";
 ?>
