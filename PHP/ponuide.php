0c3436884415f7454b5cf11ae207713c4e349a36
ivorp95 <ivor.pangos@live.com>
2023-11-14 18:21:48 +0100

    new file:   PHP/ponuide.php

---
+++ PHP/ponuide.php
@@ -0,0 +1,13 @@
+<?php
+    $server = "localhost";
+    $database = "pzi";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+    $query = "SELECT * FROM mobiteli";
+    $res = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_assoc($res)){
+        echo $row ["naziv"];
+    }
+    mysqli_close($conn);
