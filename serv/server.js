11391df210c5e26560a7cbf988bc41f296ad00c7
ivorp95 <ivor.pangos@live.com>
2023-11-21 17:45:53 +0100

    new file:   server.js

---
+++ serv/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+// set port
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+
+//node server, pokrece pozicioniranjem na web servise-node server.jr u terminalu
