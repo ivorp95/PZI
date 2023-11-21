6abc252bcb6191bb5fb28b8fee70388c692cb5a8
ivorp95 <ivor.pangos@live.com>
2023-11-21 13:35:25 +0100

    modified:   ponude.php

--- PHP/ponude.php
+++ PHP/ponude.php
@@ -8,6 +8,7 @@
     $query = "SELECT * FROM mobiteli_pzi";
     $res = mysqli_query($conn, $query);
     while($row = mysqli_fetch_assoc($res)){
+        echo "\n";
         echo $row ["NAZIV"];
         echo $row ["PROIZVODAC"];
         echo $row ["CIJENA"];
