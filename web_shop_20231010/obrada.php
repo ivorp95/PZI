be0373cfe1dcde8f1279707d354afcc602b68f88
ivorp95 <ivor.pangos@live.com>
2023-11-26 19:46:54 +0100

    popravljen php za php server /html_vjezbe	 php -S localhost:2222

--- web_shop_20231010/obrada.php
+++ web_shop_20231010/obrada.php
@@ -4,6 +4,6 @@
 <?php
 $email=$_GET['email'];
 $poruka=$_GET['poruka'];
-echo"<h1>Pozdrav ja sam PHP server!".$email. "<!h1>";
-echo"<h1>Poruka:".$poruka. "<!h1>";
+echo"<h1>Pozdrav ja sam PHP server! <br> Poslano od: ".$email. "<!h1>";
+echo"<h1>Poruka: ".$poruka. "<!h1>";
 ?>
