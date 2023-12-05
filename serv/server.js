31556ab4e251ccad1bf335eb76d0603befdeffa7
ivorp95 <ivor.pangos@live.com>
2023-12-05 15:33:59 +0100

    modified:   serv/package-lock.json 	modified:   serv/package.json 	modified:   serv/server.js

--- serv/server.js
+++ serv/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.get("/podatci", function(req,res){
+    return res.send({message:"ma bravo majmune"});
+
+});
+
 // set port
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
 });
 
-//node server, pokrece pozicioniranjem na web servise-node server.jr u terminalu
+//node server, pokrece pozicioniranjem na web servise      node server.js       u terminalu
